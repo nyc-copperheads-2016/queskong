d1fcf190515444febc128eede997617026f1edc2
David Roberts <email": null>
2015-12-12 18:42:51 -0500

    adds working ajax for take survey + controller / view validations to block saving incomplete surveys

--- app/models/possibility.rb
+++ app/models/possibility.rb
@@ -1,11 +1,19 @@
 class Possibility < ActiveRecord::Base
   belongs_to :question
   has_many :answers
+  validates :choice, presence: :true
 
-  def self.parse_possibilities(question, possibilities)
-    possibilities = possibilities.split(", ")
-      possibilities.each do |possibility|
-        question.possibilities.create(choice: possibility)
-      end
+  def self.parse_possibilities(question, possibilities_string)
+    possibilities = []
+    possibilities_string.split(", ").each do |possibility|
+      possibilities << question.possibilities.new(choice: possibility)
+    end
+    possibilities
+  end
+
+  def self.save_possibilities
+    Possibilitity.parse_possibilities.each do |possibility|
+      possibility.save
+    end
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   has_many :possibilities
   belongs_to :survey
   has_many :answers
+
+  validates :content, presence: :true
 end
